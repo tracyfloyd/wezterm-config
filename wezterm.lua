2dac75be9f9a4cc4b4c02eb6d4596b98ba1731fc
Tracy Floyd <tfloyd@reasononeinc.com>
2025-10-30 10:30:38 -0400

    Fix fonts

--- wezterm.lua
+++ wezterm.lua
@@ -15,20 +15,19 @@ config.use_fancy_tab_bar = false
 
 config.font_size = 14
 config.line_height = 1.25
---config.font = wezterm.font_with_fallback {
---  'Monaspace Krypton NF',
---}
-wezterm.font("Monaspace Krypton NF", {weight="Medium", stretch="Normal", style="Normal"}) -- /Users/tracy/Library/Fonts/MonaspaceKryptonNF-SemiWideMedium.otf, CoreText
+
+-- List all available fonts: wezterm ls-fonts --list-system
+config.font = wezterm.font("Monaspace Krypton NF", {weight="DemiBold", stretch="Normal", style="Normal"}) -- /Users/tracy/Library/Fonts/MonaspaceKryptonNF-SemiWideMedium.otf, CoreText
 
 config.color_scheme = 'Catppuccin Mocha'
 
-config.keys = {
-  {
-    key = 'w',
-    mods = 'CTRL',
-    action = wezterm.action.CloseCurrentPane { confirm = false },
-  },
-}
+-- config.keys = {
+--   {
+--     key = 'w',
+--     mods = 'CTRL',
+--     action = wezterm.action.CloseCurrentPane { confirm = false },
+--   },
+-- }
 
   -- Hide the scrollbar when there is no scrollback or alternate screen is active
 wezterm.on("update-status", function(window, pane)
